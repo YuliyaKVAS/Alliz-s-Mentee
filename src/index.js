d4793dcb60d20f544846f92e37e92f73030546a9
Yulia Kusakina <yulia_kusakina@epam.com>
2019-04-06 19:20:25 +0300

    environment for react configured

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/components/App/App';
 import './style.less';
-console.log('Helollllllllo,!!!', 'lalalla');
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/js/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => (
+    <div>I am a header</div>
+);
+
+export default Header;
