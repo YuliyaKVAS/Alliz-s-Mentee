41faa1f879067d2073634528dfc56bf4224040d0
Yulia Kusakina <yulia_kusakina@epam.com>
2019-04-20 00:08:14 +0300

    unnecessary function deleted

--- src/js/services.js
+++ src/js/services.js
@@ -1,4 +1,4 @@
-import { getData, postData } from './api';
+import { getData } from './api';
 
 export const getCourses = () => getData('courses')
   .then((result) => {
@@ -7,21 +7,4 @@ export const getCourses = () => getData('courses')
   })
   .catch(() => new Error('Server error'));
 
-
-export const authUser = (login, password) => postData('auth/login',
-  {
-    login,
-    password
-  })
-  .then(response => response.data)
-  .catch(
-    error => new Promise((resolve, reject) => {
-      if (error.response.status === 401) {
-        reject(new Error('Wrong password'));
-      } else {
-        reject(new Error('Server Error'));
-      }
-    })
-  );
-
-export default { getCourses, authUser };
+export default { getCourses };
