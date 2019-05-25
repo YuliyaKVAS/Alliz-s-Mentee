101cd2a85a1bc41adf04480341a3def9b8079024
Yulia Kusakina <yulia_kusakina@epam.com>
2019-05-26 00:23:32 +0300

    crud operations added

--- node-server/api/v1/index.js
+++ node-server/api/v1/index.js
@@ -4,10 +4,13 @@ const db = require('./queries');
 const router = express.Router();
 
 router.get('/users', db.getUsers);
+router.get('/users/:id', db.getUserById);
 
-router.get('/courses', (req, res) => {
-  res.send('v1 get courses');
-});
+router.get('/courses', db.getCourses);
+router.get('/courses/:id', db.getCourseById);
+router.put('/courses', db.createCourse);
+router.put('/courses/:id', db.updateCourse);
+router.delete('/courses/:id', db.deleteCourse);
 
 router.get('/authors', (req, res) => {
   res.send('v1 get authors');
--- node-server/api/v1/queries.js
+++ node-server/api/v1/queries.js
@@ -17,6 +17,81 @@ const getUsers = (request, response) => {
   });
 };
 
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getCourses = (request, response) => {
+  pool.query('SELECT * FROM courses', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getCourseById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM courses WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createCourse = (request, response) => {
+  const { id, name, description } = request.body;
+
+  pool.query('INSERT INTO courses (name, description) VALUES ($1, $2, $3)', [id, name, description], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`);
+  });
+};
+
+const deleteCourse = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`Course deleted with ID: ${id}`);
+  });
+};
+
+const updateCourse = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, description } = request.body;
+
+  pool.query(
+    'UPDATE courses SET name = $1, description = $2 WHERE id = $3',
+    [name, description, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`Course modified with ID: ${id}`);
+    }
+  );
+};
+
 module.exports = {
-  getUsers
+  getUsers,
+  getUserById,
+  getCourses,
+  getCourseById,
+  createCourse,
+  deleteCourse,
+  updateCourse
 };
